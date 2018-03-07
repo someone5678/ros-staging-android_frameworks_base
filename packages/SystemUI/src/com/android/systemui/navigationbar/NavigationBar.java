33afa0d5949dc0d9dccbc64840e469af865e069c
Makoto.Wada <makoto.1.wada@sony.com>
2018-03-07 18:46:04 +0900

    Change the time settings of Auto Dim Navigationbar function
    Make it possible to set auto dim of navigation bar using
    resources.
     This makes it for example possible to change the dim duration
    to avoid the color dimming being too obvious for end users.
    
    Test: Manual
    Bug: 138172569
    Change-Id: I72037f6ee9ce23b1bc1c92b00622cceb3a91f14b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -272,6 +272,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
     private boolean mIsOnDefaultDisplay;
     public boolean mHomeBlockedThisTouch;
 
+    private int mAutoDimTimeout;
+
     /**
      * When user is QuickSwitching between apps of different orientations, we'll draw a fake
      * home handle on the orientation they originally touched down to start their swipe
@@ -733,6 +735,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
 
         // Respect the latest disabled-flags.
         mCommandQueue.recomputeDisableFlags(mDisplayId, false);
+
+        mAutoDimTimeout = mContext.getResources().getInteger(R.integer.navbar_auto_dim_timeout);
 
         mNotificationShadeDepthController.addListener(mDepthListener);
         mTaskStackChangeListeners.registerTaskStackListener(mTaskStackListener);
@@ -1583,7 +1587,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         mHandler.removeCallbacks(mAutoDim);
         int state = mStatusBarStateController.getState();
         if (state != StatusBarState.KEYGUARD && state != StatusBarState.SHADE_LOCKED) {
-            mHandler.postDelayed(mAutoDim, AUTODIM_TIMEOUT_MS);
+            mHandler.postDelayed(mAutoDim, mAutoDimTimeout);
         }
     }
 
