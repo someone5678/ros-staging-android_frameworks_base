bbe4c510dd02cb312e92946c1b42e014c94c13e6
Ido Ben-Hur <idoybh2@gmail.com>
2022-10-07 02:11:14 +0300

    Screenrecord: Add an option to use HEVC
    Chose the profiles that best fit the ones we have in H.264 by spec
    
    Change-Id: I75245349f418939036ada8bb3d229ac9ad5aa078
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
@@ -63,6 +63,7 @@ class ScreenRecordPermissionDialog(
     private lateinit var stopDotSwitch: Switch
     private lateinit var lowQualitySwitch: Switch
     private lateinit var longerDurationSwitch: Switch
+    private lateinit var hevcSwitch: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setDialogTitle(R.string.screenrecord_permission_dialog_title)
@@ -112,6 +113,7 @@ class ScreenRecordPermissionDialog(
         stopDotSwitch = requireViewById(R.id.screenrecord_stopdot_switch)
         lowQualitySwitch = requireViewById(R.id.screenrecord_lowquality_switch)
         longerDurationSwitch = requireViewById(R.id.screenrecord_longer_timeout_switch)
+        hevcSwitch = requireViewById(R.id.screenrecord_hevc_switch)
         val a: ArrayAdapter<*> =
             ScreenRecordingAdapter(context, android.R.layout.simple_spinner_dropdown_item, MODES)
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -145,6 +147,7 @@ class ScreenRecordPermissionDialog(
         val showStopDot = stopDotSwitch.isChecked
         val lowQuality = lowQualitySwitch.isChecked
         val longerDuration = longerDurationSwitch.isChecked
+        val hevc = hevcSwitch.isChecked
         val startIntent =
             PendingIntent.getForegroundService(
                 userContext,
@@ -157,7 +160,8 @@ class ScreenRecordPermissionDialog(
                     captureTarget,
                     showStopDot,
                     lowQuality,
-                    longerDuration
+                    longerDuration,
+                    hevc
                 ),
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
