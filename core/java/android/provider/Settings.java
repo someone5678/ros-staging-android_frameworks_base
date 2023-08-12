edf3a4e32c443d8a66d9c3d7437bdcd6c4c65f5f
someone5678 <nemui3353@gmail.com>
2023-08-12 17:01:41 +0800

    base: Hide ADB and developer setting enable status
    * Some apps check whether ADB or developer setting is enabled
    * Return 0 to selected apps when those apps getInt
      ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
    
    mnri: move to getIntForUser to cover more scenarios
    Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4052,6 +4052,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
             String v = getStringForUser(cr, name, userHandle);
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             return parseIntSettingWithDefault(v, def);
         }
 
@@ -4082,6 +4085,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getStringForUser(cr, name, userHandle);
             return parseIntSetting(v, name);
         }
@@ -7046,6 +7052,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def, int userHandle) {
             String v = getStringForUser(cr, name, userHandle);
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             return parseIntSettingWithDefault(v, def);
         }
 
@@ -7076,6 +7085,9 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name, int userHandle)
                 throws SettingNotFoundException {
             String v = getStringForUser(cr, name, userHandle);
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             return parseIntSetting(v, name);
         }
 
@@ -17707,6 +17719,9 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getString(cr, name);
             return parseIntSettingWithDefault(v, def);
         }
@@ -17731,6 +17746,9 @@ public final class Settings {
          */
         public static int getInt(ContentResolver cr, String name)
                 throws SettingNotFoundException {
+            if (shouldHideDevStatus(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getString(cr, name);
             return parseIntSetting(v, name);
         }
@@ -20722,5 +20740,26 @@ public final class Settings {
             return null;
         }
         return packages[0];
+    }
+    
+    /**
+     * @hide
+     */
+    public static boolean shouldHideDevStatus(@NonNull String packageName, @NonNull String name) {
+        return (name.equals(Settings.Global.ADB_ENABLED) ||
+               name.equals(Settings.Global.ADB_WIFI_ENABLED) ||
+               name.equals(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED)) && !isCallerSystemApp(packageName);
+    }
+
+    /**
+     * @hide
+     */
+    private static boolean isCallerSystemApp(@NonNull String packageName) {
+        if (packageName.equals("android") 
+            || packageName.startsWith("com.android.")
+            || packageName.contains(".settings.intelligence")) {
+            return true;
+        }
+        return false;
     }
 }
