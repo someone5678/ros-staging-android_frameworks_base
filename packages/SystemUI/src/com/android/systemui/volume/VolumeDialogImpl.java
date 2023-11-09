8eb88ef7e780ce953d4d1adacd7d16dfcad719cb
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-09 08:01:27 +0800

    VolumeDialog: Ensure proper resource release during configuration changes
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -445,6 +445,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     @Override
     public void onUiModeChanged() {
         mContext.getTheme().applyStyle(mContext.getThemeResId(), true);
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
     }
 
     public void init(int windowType, Callback callback) {
@@ -2801,8 +2805,11 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onConfigurationChanged() {
-            mDialog.dismiss();
-            mConfigChanged = true;
+            if (mDialog != null) {
+                mDialog.dismiss();
+                mDialog = null;
+                mConfigChanged = true;
+            }
         }
 
         @Override
