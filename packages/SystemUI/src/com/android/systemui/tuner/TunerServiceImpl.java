bb467f9e5c4a68b122c65094c42e491a722f9e2e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-13 16:03:26 +0800

    TunerService: Chomp secure settings as well
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -172,7 +172,15 @@ public class TunerServiceImpl extends TunerService {
     }
 
     private String chomp(String key) {
-        return key.substring("system:".length());
+        String systemSettings = "system:";
+        String secureSettings = "secure:";
+        if (key.startsWith(systemSettings)) {
+            return key.substring(systemSettings.length());
+        } else if (key.startsWith(secureSettings)) {
+            return key.substring(secureSettings.length());
+        } else {
+            return key;
+        }
     }
 
     @Override
@@ -181,7 +189,7 @@ public class TunerServiceImpl extends TunerService {
             return Settings.System.getStringForUser(
                     mContentResolver, chomp(setting), mCurrentUser);
         } else {
-            return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+            return Settings.Secure.getStringForUser(mContentResolver, chomp(setting), mCurrentUser);
         }
     }
 
@@ -191,7 +199,7 @@ public class TunerServiceImpl extends TunerService {
             Settings.System.putStringForUser(
                     mContentResolver, chomp(setting), value, mCurrentUser);
         } else {
-            Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
+            Settings.Secure.putStringForUser(mContentResolver, chomp(setting), value, mCurrentUser);
         }
     }
 
@@ -201,7 +209,7 @@ public class TunerServiceImpl extends TunerService {
             return Settings.System.getIntForUser(
                     mContentResolver, chomp(setting), def, mCurrentUser);
         } else {
-            return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
+            return Settings.Secure.getIntForUser(mContentResolver, chomp(setting), def, mCurrentUser);
         }
     }
 
@@ -212,7 +220,7 @@ public class TunerServiceImpl extends TunerService {
             ret = Settings.System.getStringForUser(
                     mContentResolver, chomp(setting), mCurrentUser);
         } else {
-            ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+            ret = Secure.getStringForUser(mContentResolver, chomp(setting), mCurrentUser);
         }
         if (ret == null) return def;
         return ret;
@@ -223,7 +231,7 @@ public class TunerServiceImpl extends TunerService {
         if (isSystem(setting)) {
             Settings.System.putIntForUser(mContentResolver, chomp(setting), value, mCurrentUser);
         } else {
-            Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
+            Settings.Secure.putIntForUser(mContentResolver, chomp(setting), value, mCurrentUser);
         }
     }
 
@@ -247,7 +255,7 @@ public class TunerServiceImpl extends TunerService {
         if (isSystem(key)) {
             uri = Settings.System.getUriFor(chomp(key));
         } else {
-            uri = Settings.Secure.getUriFor(key);
+            uri = Settings.Secure.getUriFor(chomp(key));
         }
         if (!mListeningUris.containsKey(uri)) {
             mListeningUris.put(uri, key);
