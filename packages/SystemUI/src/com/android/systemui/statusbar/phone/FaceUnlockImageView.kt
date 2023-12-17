9e7ed922b095a2bbda5b6499a8debfa4cfbc8c95
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 08:36:37 +0800

    SystemUI: Implement bouncer face unlock animation
    contains face unlock fixes by jhenrique09
    * partial fixup for face unlock path 'SystemUI: Tell user when face unlock detection is running'
    * KeyguardBouncer: Don't delay showing if face auth running
    
    rmp & mnri:
    * bring in improvements and haptics
    
    this change will be squashed on next rebase for upstream purposes - parent commit: f2c5f34c23b8ddc5f95aaa239cb911e0ac3a7bd6
    
    committer: rmp
    
    approved by: hydrangea
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/FaceUnlockImageView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/FaceUnlockImageView.kt
@@ -15,50 +15,141 @@
  */
 package com.android.systemui.statusbar.phone
 
-import android.animation.ObjectAnimator
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.AsyncTask
+import android.os.Handler
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.AttributeSet
+import android.view.View
 import android.widget.ImageView
-import android.view.animation.LinearInterpolator
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.view.animation.OvershootInterpolator
 
 import com.android.systemui.R
+import com.android.systemui.statusbar.policy.ConfigurationController
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class FaceUnlockImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ImageView(context, attrs, defStyleAttr) {
 
+    private val DELAY_HIDE_DURATION = 1500
+
     enum class State {
-        SCANNING, NOT_VERIFIED, SUCCESS
+        SCANNING, NOT_VERIFIED, SUCCESS, HIDDEN
     }
 
-    private var currentState: State = State.SCANNING
+    private var currentState: State = State.HIDDEN
+    private val dismissAnimation: ObjectAnimator = createDismissAnimation()
     private val scanningAnimation: ObjectAnimator = createScanningAnimation()
-    private val successShakeAnimation: AnimatorSet = createShakeAnimation(5f)
+    private val successAnimation: ObjectAnimator = createSuccessRotationAnimation()
     private val failureShakeAnimation: AnimatorSet = createShakeAnimation(10f)
+    private val handler = Handler()
+    private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+    companion object {
+        private lateinit var configurationController: ConfigurationController
+        private var instance: FaceUnlockImageView? = null
+
+        @JvmStatic
+        fun setConfigurationController(cfgController: ConfigurationController) {
+            this.configurationController = cfgController
+        }
+
+        fun getConfigurationController(): ConfigurationController? {
+            return this.configurationController
+        }
+
+        @JvmStatic
+        fun setBouncerState(state: State) {
+            instance?.postDelayed({
+                instance?.setState(state)
+            }, 100)
+        }
+
+        @JvmStatic
+        fun setInstance(instance: FaceUnlockImageView) {
+            this.instance = instance
+        }
+    }
 
     init {
-        updateDrawable(animate = false)
+        visibility = View.GONE
+        updateFaceIconState()
+    }
+
+    public override fun onAttachedToWindow() {
+        setInstance(this)
+        getConfigurationController()?.addCallback(configurationChangedListener)
+    }
+
+    public override fun onDetachedFromWindow() {
+        getConfigurationController()?.removeCallback(configurationChangedListener)
+    }
+
+    private val configurationChangedListener =
+        object : ConfigurationController.ConfigurationListener {
+            override fun onUiModeChanged() {
+                updateColor()
+            }
+            override fun onThemeChanged() {
+                updateColor()
+            }
+    }
+
+    fun updateColor() {
+        val isDark = (context.resources.configuration.uiMode
+                and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+        val darkColor = context.getColor(R.color.island_background_color_dark)
+        val lightColor = context.getColor(R.color.island_background_color_light)
+        if ((this as? View)?.id == R.id.bouncer_face_unlock_icon) {
+            imageTintList = ColorStateList.valueOf(if (isDark) lightColor else darkColor)
+        } else {
+            imageTintList = ColorStateList.valueOf(Color.parseColor("#FFFFFF"))
+        }
     }
 
     fun setState(state: State) {
         if (currentState != state) {
             currentState = state
-            updateDrawable(animate = true)
+            updateFaceIconState()
             handleAnimationForState(state)
         }
     }
 
-    private fun updateDrawable(animate: Boolean) {
+    private fun updateFaceIconState() {
         setImageResource(when (currentState) {
             State.SCANNING -> R.drawable.face_scanning
             State.NOT_VERIFIED -> R.drawable.face_not_verified
             State.SUCCESS -> R.drawable.face_success
+            State.HIDDEN -> 0
         })
     }
+
+    private fun startOvershootAnimation() {
+        val overshootAnimator = ObjectAnimator.ofPropertyValuesHolder(
+            this,
+            PropertyValuesHolder.ofFloat("scaleX", 0f, 1.2f, 1f),
+            PropertyValuesHolder.ofFloat("scaleY", 0f, 1.2f, 1f)
+        )
+        overshootAnimator.duration = 500
+        overshootAnimator.interpolator = OvershootInterpolator(1.5f)
+        overshootAnimator.start()
+    }
+
 
     private fun createScanningAnimation(): ObjectAnimator {
         val scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.2f, 1f)
@@ -68,6 +159,14 @@ class FaceUnlockImageView @JvmOverloads constructor(
         scanningAnimator.repeatCount = ObjectAnimator.INFINITE
         scanningAnimator.interpolator = LinearInterpolator()
         return scanningAnimator
+    }
+
+    private fun createSuccessRotationAnimation(): ObjectAnimator {
+        val flipRotation = PropertyValuesHolder.ofFloat("rotationY", 0f, 360f)
+        val flipAnimator = ObjectAnimator.ofPropertyValuesHolder(this, flipRotation)
+        flipAnimator.duration = 800
+        flipAnimator.interpolator = AccelerateDecelerateInterpolator()
+        return flipAnimator
     }
 
     private fun createShakeAnimation(amplitude: Float): AnimatorSet {
@@ -80,22 +179,61 @@ class FaceUnlockImageView @JvmOverloads constructor(
         return animatorSet
     }
 
+    private fun createDismissAnimation(): ObjectAnimator {
+        return ObjectAnimator.ofPropertyValuesHolder(
+            this,
+            PropertyValuesHolder.ofFloat("scaleX", 1f, 0f),
+            PropertyValuesHolder.ofFloat("scaleY", 1f, 0f)
+        ).apply {
+            duration = 500
+            interpolator = AccelerateDecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    visibility = View.GONE
+                }
+            })
+        }
+    }
+
+    private fun vibrate(effect: Int) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val vibrationEffect = VibrationEffect.createPredefined(effect)
+            vibrator.vibrate(vibrationEffect)
+        }
+    }
+
     private fun handleAnimationForState(state: State) {
         when (state) {
             State.SCANNING -> {
+                visibility = View.VISIBLE
                 failureShakeAnimation.cancel()
-                successShakeAnimation.cancel()
-                scanningAnimation.start()
+                successAnimation.cancel()
+                startOvershootAnimation()
+                handler.post({ scanningAnimation.start() })
             }
             State.NOT_VERIFIED -> {
                 scanningAnimation.cancel()
-                successShakeAnimation.cancel()
+                successAnimation.cancel()
                 failureShakeAnimation.start()
+                vibrate(VibrationEffect.EFFECT_DOUBLE_CLICK)
             }
             State.SUCCESS -> {
                 scanningAnimation.cancel()
                 failureShakeAnimation.cancel()
-                successShakeAnimation.start()
+                successAnimation.start()
+                vibrate(VibrationEffect.EFFECT_CLICK)
+            }
+            State.HIDDEN -> {
+                failureShakeAnimation.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        handler.post({ dismissAnimation.start() })
+                    }
+                })
+                successAnimation.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        handler.post({ dismissAnimation.start() })
+                    }
+                })
             }
         }
     }
